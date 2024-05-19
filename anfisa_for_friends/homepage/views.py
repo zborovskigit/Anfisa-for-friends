e02d51d9253082e4f9de837afa664f70cb050545
Ivan <me@zborovski.ru>
2024-05-19 14:47:42 +0300

    Updated homepage view and index.html

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+        'title', 'id', 'description'
+    ).filter(is_on_main=True)
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
